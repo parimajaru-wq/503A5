d1da4a81d27ade8a385e1e8caca2c9bd11b1dd78
ปาริมา จารุธานีวงศ์ <papuean@MacBookAir-5.local>
2026-08-25 13:51:33 +0700

    Finish task6

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+const hospitals = require ('./routes/hospitals');
+
 dotenv.config({path:'./config/config.env'});
 
-const app=express()
+const app=express();
 
-app.get('/', (req,res) => {
-    res.send('<h1>Hello from express</h1>');
-    //res.send({name:'Brad'});
-    //res.json({name:'Brad'});
-    //res.sendStatus(400);
-    //res.status(400).json({success:false});
-    //res.status(200).json({success:true, data:{id:1}})
-});
+app.use('/api/v1/hospitals',hospitals);
 
 const PORT=process.env.PORT || 5003;
+
 app.listen(PORT, console.log('Server running in', process.env.NODE_ENV,'mode on port', PORT));
